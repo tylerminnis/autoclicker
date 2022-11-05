131779133cb5e06d3125d54f67d81a93fed5be99
tylerminnis <tylerminnis10@outlook.com>
2022-11-05 15:12:02 -0400

    fixed start/stop bug

--- clicker.py
+++ clicker.py
@@ -2,14 +2,15 @@ import time
 import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
-#what if i said that it doesn't work fully the way i want it to
+
 #controls
 delay = 0.0001
 button = Button.left
 start_stop_key = KeyCode(char = 'a')
-stop_key = KeyCode(char = 'b')
+stop_key = KeyCode(char = 'c')
 
 class ClickMouse(threading.Thread):
+
     def __init__(self, delay, button):
         super(ClickMouse, self).__init__()
         self.delay = delay
@@ -20,6 +21,7 @@ class ClickMouse(threading.Thread):
     #doesn't work for some reason
     def start_clicking(self):
         self.running = True
+
     #doesn't work for some reason
     def stop_clicking(self):
         self.running = False
@@ -30,8 +32,9 @@ class ClickMouse(threading.Thread):
 
     def run(self):
         while self.program_running:
-            mouse.click(self.button)
-            time.sleep(self.delay)
+            while self.running:
+                mouse.click(self.button)
+                time.sleep(self.delay)
         time.sleep(0.1)
 
 mouse = Controller()
@@ -46,10 +49,10 @@ def on_press(key):
         else:
             click_thread.start_clicking()
             print('Started running!')
-
     elif key == stop_key:
         click_thread.exit()        
         listener.stop()
         print('Exited program!')
+
 with Listener(on_press=on_press) as listener:
     listener.join()
